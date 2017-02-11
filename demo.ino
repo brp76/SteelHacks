c7a742c71d5e0692fd7cc68a4816721d2df8608b
Eric <erg52@pitt.edu>
2017-02-11 10:53:20 -0500

    fixed mapping... ?

--- demo.ino
+++ demo.ino
@@ -3,7 +3,8 @@
 #define X A0
 #define Y A1
 
-int yPairs[2][8] = {{7,6,5,4,3,2,1,0},{8,9,10,11,12,13,14,15}};
+//int yPairs[2][8] = {{7,6,5,4,3,2,1,0},{8,9,10,11,12,13,14,15}};
+int yPairs[2][8] = {{0,1,2,3,4,5,6,7},{15,14,13,12,11,10,9,8}};
 int xPairs[2][8] = {{3,2,1,0,15,14,13,12},{4,5,6,7,8,9,10,11}};
 int xPos,yPos;
 
@@ -24,10 +25,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  strip.clear();
   xPos = analogRead(X);
   yPos = analogRead(Y);
-  xPos = xPos/128;
-  yPos = yPos/128;
+  //xPos = xPos/128;
+  //yPos = yPos/128;
+  xPos = map(xPos, 0, 1023, 0, 7);
+  yPos = map(yPos, 0, 1023, 0, 7);
   a = xPairs[0][xPos];  //first of two lights at given x position
   b = xPairs[1][xPos];  //pairing light at given x position
   c = yPairs[0][yPos];  //pair of ligts at given y position
@@ -38,7 +42,7 @@ void loop() {
       if(demoPairs[0][i] == a){
         strip.setPixelColor(demoPairs[1][i],strip.Color(0,255,0));
         strip.show();
-        delay(50);
+        delay(100);
         strip.setPixelColor(demoPairs[1][i],strip.Color(0,0,0));
         strip.setPixelColor(a,strip.Color(0,0,0));
         strip.show();
@@ -47,7 +51,7 @@ void loop() {
       if(demoPairs[1][i] == a){
         strip.setPixelColor(demoPairs[0][i],strip.Color(0,255,0));
         strip.show();
-        delay(50);
+        delay(100);
         strip.setPixelColor(demoPairs[0][i],strip.Color(0,0,0));
         strip.setPixelColor(a,strip.Color(0,0,0));
         strip.show();
@@ -62,7 +66,7 @@ void loop() {
       if(demoPairs[0][i] == b){
         strip.setPixelColor(demoPairs[1][i],strip.Color(0,255,0));
         strip.show();
-        delay(50);
+        delay(100);
         strip.setPixelColor(demoPairs[1][i],strip.Color(0,0,0));
         strip.setPixelColor(b,strip.Color(0,0,0));
         strip.show();
@@ -71,7 +75,7 @@ void loop() {
       if(demoPairs[1][i] == b){
         strip.setPixelColor(demoPairs[0][i],strip.Color(0,255,0));
         strip.show();
-        delay(50);
+        delay(100);
         strip.setPixelColor(demoPairs[1][i],strip.Color(0,0,0));
         strip.setPixelColor(b,strip.Color(0,0,0));
         strip.show();
