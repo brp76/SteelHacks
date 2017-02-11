16a25dbe6546ae3881abb76b5decc89c05703088
John Dott <jkd28@pitt.edu>
2017-02-11 14:51:16 -0500

    Added the base functionality of the Victory Wheel Game

---
+++ victorywheel.ino
@@ -0,0 +1,59 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 7
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+
+uint8_t count = 0;
+uint8_t wait = 250; 
+
+void setup() {
+  strip.setBrightness(100);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+}
+
+void loop() {
+  wait = wait-(++count);
+  // Fill the dots one after the other with a color
+  colorWipe(strip.Color(255, 0, 0), wait); // Red
+  strip.clear();
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+    for(uint16_t i=0; i<strip.numPixels(); i++) {    
+      if(i == strip.numPixels()-1){
+        strip.setPixelColor(i,(strip.Color(0,255,0)));
+      } else {
+        strip.setPixelColor(i, c);
+      }
+      strip.show();
+      delay(wait);
+    }
+}
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
